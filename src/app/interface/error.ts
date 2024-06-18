8ee247439917c4e5d08b6291d1e3a44a190d5ab0
Rakib Hassan <mailrakibhassan@gmail.com>
2024-06-18 10:43:10 +0600

    zod error handling made simplified

---
+++ src/app/interface/error.ts
@@ -0,0 +1,4 @@
+export type ErrorSources = {
+    path: string | number;
+    message: string;
+}[];
--- src/app/middlewares/global-error.ts
+++ src/app/middlewares/global-error.ts
@@ -1,15 +1,47 @@
-import { NextFunction, Request, Response } from 'express';
-import AppError from '../errors/AppError';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { ErrorSources } from '../interface/error';
+import config from '../config';
 
-export default function globalError(
-    error: AppError,
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
-    res.status(error.statusCode).json({
+const handleZodError = (error: ZodError) => {
+    const errorSources: ErrorSources = error.issues.map((issue: ZodIssue) => ({
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+    }));
+    return {
+        statusCode: 400,
+        message: 'Validation error',
+        errorSources,
+    };
+};
+
+const globalError: ErrorRequestHandler = (error, req, res, next) => {
+    // setting default values
+    let statusCode = error.statusCode || 500;
+    let message = error.message || 'Something went wrong!';
+
+    let errorSources: ErrorSources = [
+        {
+            path: '',
+            message: 'Something went wrong!',
+        },
+    ];
+
+    if (error instanceof ZodError) {
+        const simplifiedError = handleZodError(error);
+
+        // override default values
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
+    }
+
+    res.status(statusCode).json({
         success: false,
-        message: error.message || 'Something went wrong!',
-        error,
+        message,
+        errorSources,
+        stack: config.NODE_ENV === 'development' ? error.stack : null,
     });
-}
+};
+
+export default globalError;
