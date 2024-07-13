dbdc3d1665d155c2004e157571d2c92fc44228c8
Rakib Hassan <mailrakibhassan@gmail.com>
2024-07-13 17:45:31 +0600

    password matching function applied

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,36 @@
-import { TLoginCredentials } from "./auth.interface";
-
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
+import { TLoginCredentials } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (loginCredentials: TLoginCredentials) => {
+    // check if user doesn't exist in the database
+    const isUserExist = await User.findOne({ id: loginCredentials.id });
+
+    if (!isUserExist) {
+        throw new AppError(httpStatus.NOT_FOUND, 'The user is not found!');
+    }
+    // checking if the user is deleted
+    if (isUserExist.isDeleted) {
+        throw new AppError(httpStatus.FORBIDDEN, 'The user is deleted!');
+    }
+    // check if user status is "blocked"
+    if (isUserExist.status === 'blocked') {
+        throw new AppError(httpStatus.FORBIDDEN, 'The user is blocked!');
+    }
+
+    // check that password is matched to the hased password
+    const isPasswordMatched = await bcrypt.compare(
+        loginCredentials.password,
+        isUserExist.password
+    );
+
+    // Access Granted: Send Access Token, Refresh Token
 
     return {};
-}
+};
 
 export const AuthServices = {
     loginUser,
-}
+};
