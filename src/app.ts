b5c6e3946f2d515d84304e081e8ae7049089541b
Rakib Hassan <mailrakibhassan@gmail.com>
2024-08-16 07:42:29 +0600

    added cookieParser package

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import globalError from './app/middlewares/global-error';
 import notFound from './app/middlewares/not-found';
 import { router } from './app/routes';
 import httpStatus from 'http-status';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.get('/', async (req: Request, res: Response) => {
     // following code produces unhandledRejection
