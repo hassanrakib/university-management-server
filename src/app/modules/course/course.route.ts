33d0dbdb96bf14b9a4b0004d306308873091ad2e
Rakib Hassan <mailrakibhassan@gmail.com>
2024-08-22 08:45:04 +0600

    module-17 finished

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -2,17 +2,24 @@ import express from 'express';
 import { CourseController } from './course.controller';
 import validateRequest from '../../middlewares/validate-request';
 import { CourseValidations } from './course.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 router.post(
     '/create-course',
     validateRequest(CourseValidations.createCourseSchema),
+    auth(USER_ROLE.admin),
     CourseController.createCourse
 );
 router.get('/', CourseController.getAllCourses);
-router.get('/:courseId', CourseController.getCourseById);
-router.patch('/:courseId', CourseController.updateCourseById);
-router.delete('/:courseId', CourseController.deleteCourseById);
+router.get(
+    '/:courseId',
+    auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+    CourseController.getCourseById
+);
+router.patch('/:courseId', auth(USER_ROLE.admin), CourseController.updateCourseById);
+router.delete('/:courseId', auth(USER_ROLE.admin), CourseController.deleteCourseById);
 router.put(
     '/:courseId/assign-faculties',
     validateRequest(CourseValidations.updateCourseFacultySchema),
