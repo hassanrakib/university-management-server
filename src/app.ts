001e6585d5a7be203986a4874b053ba723b4f2d8
Rakib Hassan <mailrakibhassan@gmail.com>
2024-11-09 18:16:38 +0600

    auth.controller.ts - removed properties of res.cookie options in loginUser

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ const app: Application = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
 app.use(cookieParser());
+app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
 
 app.get('/', async (req: Request, res: Response) => {
     // following code produces unhandledRejection
